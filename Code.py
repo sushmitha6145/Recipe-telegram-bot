e191beec1dc5ed7df4006166352374b32e95f260
sushmitha6145 <128017237+sushmitha6145@users.noreply.github.com>
2023-07-02 14:17:31 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,6 +1,7 @@
 import csv
 import requests
 import json
+import random
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot_token = '6367686078:AAFlTgxSDN2j2AW83f9Smlz_FZBaMTgZ2GE'
@@ -9,7 +10,7 @@ def extract_recipe_info(csv_file, recipe_name, chat_id):
     with open(csv_file, 'r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            if row['RecipeName'] == recipe_name:
+            if row['RecipeName'].lower() == recipe_name.lower():
                 response = f"Recipe: {row['RecipeName']}\n\nIngredients:\n"
                 ingredients = row['Ingredients'].split(',')
                 for ingredient in ingredients:
@@ -33,10 +34,9 @@ def extract_recipe_info(csv_file, recipe_name, chat_id):
 
                 # Send the response message to the specified chat_id
                 send_message(chat_id, response)
-                return True
+                return
 
     send_message(chat_id, f"Recipe '{recipe_name}' not found in the dataset.")
-    return False
 
 def send_message(chat_id, text):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
@@ -55,21 +55,34 @@ def handle_updates(updates):
                 text = message['text']
                 # Check if the user entered the /start command
                 if text == '/start':
-                    welcome_message = "Welcome! I'm a chatbot capable of giving recipes.\n" \
-                                      "Please enter the dish name in this format: '<dishname> Recipe'"
+                    welcome_message = "Welcome! I'm a chatbot capable of giving recipes.\n"\
+                                      "\nPlease enter the dish name in this format: '<dishname> Recipe'\n\nEnter /some_recipes to get 25 random recipes for every click"
                     send_message(chat_id, welcome_message)
+                elif text == '/some_recipes':
+                    # Display a unique set of 25 recipes from the dataset
+                    recipe_list = get_some_recipes(recipe_dataset_file, 25)
+                    recipe_list_message = "Here are 25 unique recipes from the dataset:\n\n"
+                    for i, recipe in enumerate(recipe_list, start=1):
+                        recipe_list_message += f"{i}. {recipe}\n"
+                    send_message(chat_id, recipe_list_message)
                 else:
                     # Extract and display the recipe information
-                    recipe_sent = extract_recipe_info(recipe_dataset_file, text, chat_id)
-                    if not recipe_sent:
-                        send_message(chat_id, "Please enter a valid recipe name.")
+                    extract_recipe_info(recipe_dataset_file, text, chat_id)
             else:
                 send_message(chat_id, "Please enter the recipe name.")
+
+def get_some_recipes(csv_file, num_recipes):
+    recipes = []
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            recipes.append(row['RecipeName'])
+    unique_recipes = random.sample(recipes, num_recipes)
+    return unique_recipes
 
 # Example usage
 recipe_dataset_file = 'IndianFoodDatasetCSV.csv'
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
 offset = None
 
 while True:
