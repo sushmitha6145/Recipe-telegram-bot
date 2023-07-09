f3ba17350df3bcf43175ce4bd187bfd4e61510f6
sushmitha6145 <128017237+sushmitha6145@users.noreply.github.com>
2023-07-09 10:50:54 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,7 +1,7 @@
 import csv
 import requests
-import json
 import random
+import streamlit as st
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot_token = '6367686078:AAFlTgxSDN2j2AW83f9Smlz_FZBaMTgZ2GE'
@@ -46,31 +46,6 @@ def send_message(chat_id, text):
     }
     requests.post(url, json=params)
 
-def handle_updates(updates):
-    for update in updates:
-        if 'message' in update:
-            message = update['message']
-            chat_id = message['chat']['id']
-            if 'text' in message:
-                text = message['text']
-                # Check if the user entered the /start command
-                if text == '/start':
-                    welcome_message = "Welcome! I'm a chatbot capable of giving recipes.\n"\
-                                      "\nPlease enter the dish name in this format: '<dishname> Recipe'"
-                    send_message(chat_id, welcome_message)
-                elif text == '/some_recipes':
-                    # Display a unique set of 25 recipes from the dataset
-                    recipe_list = get_some_recipes(recipe_dataset_file, 25)
-                    recipe_list_message = "Here are 25 unique recipes from the dataset:\n\n"
-                    for i, recipe in enumerate(recipe_list, start=1):
-                        recipe_list_message += f"{i}. {recipe}\n"
-                    send_message(chat_id, recipe_list_message)
-                else:
-                    # Extract and display the recipe information
-                    extract_recipe_info(recipe_dataset_file, text, chat_id)
-            else:
-                send_message(chat_id, "Please enter the recipe name.")
-
 def get_some_recipes(csv_file, num_recipes):
     recipes = []
     with open(csv_file, 'r', encoding='utf-8') as file:
@@ -83,17 +58,24 @@ def get_some_recipes(csv_file, num_recipes):
 # Example usage
 recipe_dataset_file = 'IndianFoodDatasetCSV.csv'
 
-offset = None
+def main():
+    st.title("Recipe Chatbot")
 
-while True:
-    url = f"https://api.telegram.org/bot{bot_token}/getUpdates"
-    params = {'offset': offset, 'timeout': 60}
-    response = requests.get(url, params=params).json()
+    st.markdown("Welcome! I'm a chatbot capable of providing recipes.")
 
-    if 'result' in response:
-        updates = response['result']
-        if updates:
-            offset = updates[-1]['update_id'] + 1
-            handle_updates(updates)
-    else:
-        print('Error occurred while retrieving updates.')
+    user_input = st.text_input("Enter the dish name in this format: '<dishname> Recipe'")
+
+    if user_input:
+        if user_input == '/some_recipes':
+            # Display a unique set of 25 recipes from the dataset
+            recipe_list = get_some_recipes(recipe_dataset_file, 25)
+            recipe_list_message = "Here are 25 unique recipes from the dataset:\n\n"
+            for i, recipe in enumerate(recipe_list, start=1):
+                recipe_list_message += f"{i}. {recipe}\n"
+            st.markdown(recipe_list_message)
+        else:
+            # Extract and display the recipe information
+            extract_recipe_info(recipe_dataset_file, user_input, 1)
+
+if __name__ == "__main__":
+    main()
